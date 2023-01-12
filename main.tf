514d9d3334ebd0de0131c32607d58b28ffc653ea
Mostafa Abdalla <mostafa.abdallah1@vodafone.com>
2023-01-12 19:54:15 +0200

    creating ec2 and installing jenkins

---
+++ main.tf
@@ -0,0 +1,61 @@
+resource "aws_instance" "default" {
+  ami           = "ami-0b5eea76982371e91"
+  instance_type = "t2.micro"
+
+  key_name = "devops_project"
+
+  security_groups = [aws_security_group.default.name]
+
+  user_data = <<EOF
+    #!/bin/bash
+    sudo yum update -y
+    sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+    sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+    sudo yum upgrade
+    sudo amazon-linux-extras install java-openjdk11 -y
+    sudo yum install jenkins -y
+    sudo systemctl enable jenkins
+    sudo systemctl start jenkins
+    EOF
+
+  tags = {
+    Name = "Jenkins_Server"
+  }
+}
+
+resource "aws_security_group" "default" {
+  name        = "Jenkins_sg"
+  description = "Jenkins_sg"
+  vpc_id      = "vpc-0c0c7be4f059ab962"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+output "public_dns_name" {
+     value = aws_instance.default.public_dns
+}
