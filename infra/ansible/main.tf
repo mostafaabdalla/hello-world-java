ecd7118370145f1f0c3853efe5edf01c440de010
Mostafa Abdalla <mostafa.abdallah1@vodafone.com>
2023-01-18 23:41:46 +0200

    setup ansible server

---
+++ infra/ansible/main.tf
@@ -0,0 +1,26 @@
+module "ansible-server" {
+  source = "../modules/ec2"
+
+  ami = "ami-0b5eea76982371e91"
+
+  instance_type = "t2.micro"
+
+
+  tags = {
+    Name = "ansible-server"
+  }
+  security_group_name = "ansible-server-sg"
+  key_name            = "devops_project"
+  user_data           = <<EOF
+  #!/bin/bash
+  hostnamectl set-hostname ansible-server
+  yum update -y
+  amazon-linux-extras install ansible2
+  useradd ansadmin
+  usermod -aG wheel ansadmin
+  EOF
+}
+
+output "instance_public_dns_name" {
+  value = module.docker-host.instance_public_dns_name
+}
