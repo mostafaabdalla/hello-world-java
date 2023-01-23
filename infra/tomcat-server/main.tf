c0eeb78dfaa392365ff7c262c53bc4ed44f623ad
Mostafa Abdalla <mostafa.abdallah1@vodafone.com>
2023-01-23 17:50:57 +0200

    installed tomcat server using the ec2 module

--- infra/tomcat-server/main.tf
+++ infra/tomcat-server/main.tf
@@ -1,10 +1,9 @@
-resource "aws_instance" "tomcat" {
-  ami           = "ami-0b5eea76982371e91"
-  instance_type = "t2.micro"
-
-  key_name = "devops_project"
-
-  security_groups = [aws_security_group.tomcat-sg.name]
+module "tomcat-server" {
+  source              = "../modules/ec2"
+  ami                 = "ami-0b5eea76982371e91"
+  instance_type       = "t2.micro"
+  key_name            = "devops_project"
+  security_group_name = "tomcat-server-sg"
 
   user_data = <<EOF
     #!/bin/bash
@@ -27,39 +26,6 @@ resource "aws_instance" "tomcat" {
     Name = "Tomcat_Server"
   }
 }
-
-resource "aws_security_group" "tomcat-sg" {
-  name        = "Tomcat_sg"
-  description = "Tomcat_sg"
-  vpc_id      = "vpc-0c0c7be4f059ab962"
-
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "allow_tls"
-  }
-}
-
-output "tomcat_public_dns_name" {
-  value = aws_instance.tomcat.public_dns
+output "instance_public_dns_name" {
+  value = module.tomcat-server.instance_public_dns_name
 }
