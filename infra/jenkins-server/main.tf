9d6881d506cfc8af3abc2b21288ee27f3bf49bcb
Mostafa Abdalla <mostafa.abdallah1@vodafone.com>
2023-01-23 17:20:03 +0200

    used ec2 module to create jenkins server

--- infra/jenkins-server/main.tf
+++ infra/jenkins-server/main.tf
@@ -1,22 +1,20 @@
-resource "aws_instance" "jenkins" {
-  ami           = "ami-0b5eea76982371e91"
-  instance_type = "t2.micro"
-
-  key_name = "devops_project"
-
-  security_groups = [aws_security_group.jenkins-sg.name]
-
-  user_data = <<EOF
+module "jenkins-server" {
+  source              = "../modules/ec2"
+  ami                 = "ami-0b5eea76982371e91"
+  instance_type       = "t2.micro"
+  key_name            = "devops_project"
+  security_group_name = "jenkins-server-sg"
+  user_data           = <<EOF
     #!/bin/bash
-    sudo yum update -y
-    sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    yum update -y
+    wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
-    sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-    sudo yum upgrade
-    sudo amazon-linux-extras install java-openjdk11 -y
-    sudo yum install jenkins -y
-    sudo systemctl enable jenkins
-    sudo systemctl start jenkins
+    rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+    yum upgrade
+    amazon-linux-extras install java-openjdk11 -y
+    yum install jenkins -y
+    systemctl enable jenkins
+    systemctl start jenkins
     ###########################
     # install git
     sudo yum install git -y
@@ -32,48 +30,13 @@ M2=/opt/maven/bin
 JAVA_HOME=/usr/lib/jvm/java-11-openjdk-11.0.16.0.8-1.amzn2.0.1.x86_64" >> ~/.bash_profile
     echo "PATH=$PATH:$HOME/bin:/opt/maven:/opt/maven/bin:/usr/lib/jvm/java-11-openjdk-11.0.16.0.8-1.amzn2.0.1.x86_64" >> ~/.bash_profile
     echo "export PATH" >> ~/.bash_profile
-
     sudo source ~/.bash_profile
-    
     EOF
-
   tags = {
     Name = "Jenkins_Server"
   }
 }
 
-resource "aws_security_group" "jenkins-sg" {
-  name        = "Jenkins_sg"
-  description = "Jenkins_sg"
-  vpc_id      = "vpc-0c0c7be4f059ab962"
-
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "allow_tls"
-  }
-}
-
-output "jenkins_public_dns_name" {
-  value = aws_instance.jenkins.public_dns
+output "instance_public_dns_name" {
+  value = module.jenkins-server.instance_public_dns_name
 }
