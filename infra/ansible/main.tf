3ec69efaa5f46492ade9503f17b5a6b084e4df49
Mostafa Abdalla <mostafa.abdallah1@vodafone.com>
2023-01-23 18:59:35 +0200

    reorganized the repo files

--- infra/ansible/main.tf
+++ infra/ansible/main.tf
@@ -1,14 +1,7 @@
 module "ansible-server" {
-  source = "../modules/ec2"
-
-  ami = "ami-0b5eea76982371e91"
-
-  instance_type = "t2.micro"
-
-
-  tags = {
-    Name = "ansible-server"
-  }
+  source              = "../modules/ec2"
+  ami                 = "ami-0b5eea76982371e91"
+  instance_type       = "t2.micro"
   security_group_name = "ansible-server-sg"
   key_name            = "devops_project"
   user_data           = <<EOF
@@ -24,7 +17,12 @@ module "ansible-server" {
   mkdir /opt/docker
   chown ansadmin:ansadmin /opt/docker
   EOF
+  tags = {
+    Name = "ansible-server"
+  }
 }
+
+
 
 output "instance_public_dns_name" {
   value = module.ansible-server.instance_public_dns_name
--- infra/docker-host/main.tf
+++ infra/docker-host/main.tf
@@ -1,14 +1,7 @@
 module "docker-host" {
-  source = "../modules/ec2"
-
-  ami = "ami-0b5eea76982371e91"
-
-  instance_type = "t2.micro"
-
-
-  tags = {
-    Name = "docker-host"
-  }
+  source              = "../modules/ec2"
+  ami                 = "ami-0b5eea76982371e91"
+  instance_type       = "t2.micro"
   security_group_name = "docker-host-sg"
   key_name            = "devops_project"
   user_data           = <<EOF
@@ -22,6 +15,10 @@ module "docker-host" {
   mkdir /opt/docker
   chown dockeradmin:dockeradmin /opt/docker
   EOF
+  tags = {
+    Name = "docker-host"
+  }
+
 }
 
 output "instance_public_dns_name" {
